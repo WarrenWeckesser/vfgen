44325f9ac5538547d1c61830695f05b34498fc3a
Warren Weckesser <warren.weckesser@gmail.com>
2013-09-13 09:15:16 -0400

    gsl: update style of generated code

--- src/vf_gsl.cpp
+++ src/vf_gsl.cpp
@@ -94,7 +94,7 @@ void VectorField::PrintGSL(map<string,string> options)
     fout << endl;
     fout << "int " << Name() << "_vf(double t, const double y_[], double f_[], void *params)" << endl;
     pout << "int " << Name() << "_vf(double, const double [], double [], void *);" << endl;
-    fout << "    {" << endl;
+    fout << "{" << endl;
     if (HasPi) {
         fout << "    const double Pi = M_PI;\n";
     }
@@ -123,7 +123,7 @@ void VectorField::PrintGSL(map<string,string> options)
     }
     fout << endl;
     fout << "    return GSL_SUCCESS;" << endl;
-    fout << "    }" << endl;
+    fout << "}" << endl;
     fout << endl;
     //
     // Print the Jacobian function.
@@ -134,7 +134,7 @@ void VectorField::PrintGSL(map<string,string> options)
     fout << endl;
     fout << "int " << Name() << "_jac(double t, const double y_[], double *jac_, double *dfdt_, void *params)" << endl;
     pout << "int " << Name() << "_jac(double, const double [], double *, double *, void *);" << endl;
-    fout << "    {" << endl;
+    fout << "{" << endl;
     if (HasPi) {
         fout << "    const double Pi = M_PI;\n";
     }
@@ -168,7 +168,7 @@ void VectorField::PrintGSL(map<string,string> options)
     }
     fout << endl;
     fout << "    return GSL_SUCCESS;" << endl;
-    fout << "    }" << endl;
+    fout << "}" << endl;
     //
     // Print the function that computes the Jacobian with respect
     // to the parameters.
@@ -180,7 +180,7 @@ void VectorField::PrintGSL(map<string,string> options)
     fout << endl;
     fout << "int " << Name() << "_jacp(double t, const double y_[], double *jacp_, void *params)" << endl;
     pout << "int " << Name() << "_jacp(double, const double [], double *, void *);" << endl;
-    fout << "    {" << endl;
+    fout << "{" << endl;
     if (HasPi) {
         fout << "    const double Pi = M_PI;\n";
     }
@@ -209,7 +209,7 @@ void VectorField::PrintGSL(map<string,string> options)
     }
     fout << endl;
     fout << "    return GSL_SUCCESS;" << endl;
-    fout << "    }" << endl;
+    fout << "}" << endl;
 
     if (options["func"] == "yes") {
         //
@@ -223,7 +223,7 @@ void VectorField::PrintGSL(map<string,string> options)
             fout << endl;
             fout << "double " << Name() << "_" << funcname_list[n] << "(double t, const double y_[], void *params)" << endl;
             pout << "double " << Name() << "_" << funcname_list[n] << "(double, const double [], void *);" << endl;
-            fout << "    {" << endl;
+            fout << "{" << endl;
             if (HasPi) {
                 fout << "    const double Pi = M_PI;\n";
             }
@@ -248,7 +248,7 @@ void VectorField::PrintGSL(map<string,string> options)
                 fout << endl;
             }
             fout << "    return " << funcformula_list[n] << ";" << endl;
-            fout << "    }" << endl;
+            fout << "}" << endl;
         }
     }
 
@@ -292,45 +292,51 @@ void VectorField::PrintGSL(map<string,string> options)
         tout << endl;
         tout << endl << endl;
         tout << "int use(int argc, char *argv[], int nv, char *vname[], double y_[], int np, char *pname[], double p_[])\n" ;
-        tout << "    {\n" ;
+        tout << "{\n" ;
         tout << "    int i;\n" ;
-        tout << "    printf(\"use: %s [options]\\n\",argv[0]);\n" ;
+        tout << "    printf(\"use: %s [options]\\n\", argv[0]);\n" ;
         tout << "    printf(\"options:\\n\");\n" ;
         tout << "    printf(\"    -h    Print this help message.\\n\");\n" ;
-        tout << "    for (i = 0; i < nv; ++i)\n" ;
-        tout << "        printf(\"    %s=<initial_condition>   Default value is %e\\n\",vname[i],y_[i]);\n";
-        tout << "    for (i = 0; i < np; ++i)\n" ;
-        tout << "        printf(\"    %s=<parameter_value>   Default value is %e\\n\",pname[i],p_[i]);\n";
+        tout << "    for (i = 0; i < nv; ++i) {\n" ;
+        tout << "        printf(\"    %s=<initial_condition>   Default value is %e\\n\", vname[i], y_[i]);\n";
+        tout << "    }\n";
+        tout << "    for (i = 0; i < np; ++i) {\n" ;
+        tout << "        printf(\"    %s=<parameter_value>   Default value is %e\\n\", pname[i], p_[i]);\n";
+        tout << "    }\n";
         tout << "    printf(\"    abserr=<absolute_error_tolerance>\\n\");\n" ;
         tout << "    printf(\"    relerr=<relative_error_tolerance>\\n\");\n" ;
         tout << "    printf(\"    stoptime=<stop_time>\\n\");\n" ;
         tout << "    return 0;\n";
-        tout << "    }\n" ;
+        tout << "}\n" ;
         tout << endl << endl;
         tout << "int assign(char *str[], int ns, double v[], char *a)\n" ;
-        tout << "    {\n" ;
+        tout << "{\n" ;
         tout << "    int i;\n" ;
         tout << "    char name[256];\n" ;
         tout << "    char *e;\n" ;
         tout << "\n" ;
         tout << "    e = strchr(a,'=');\n" ;
-        tout << "    if (e == NULL)\n" ;
+        tout << "    if (e == NULL) {\n" ;
         tout << "        return(-1);\n" ;
+        tout << "    }\n";
         tout << "    *e = '\\0';\n" ;
-        tout << "    strcpy(name,a);\n" ;
+        tout << "    strcpy(name, a);\n" ;
         tout << "    *e = '=';\n" ;
         tout << "    ++e;\n" ;
-        tout << "    for (i = 0; i < ns; ++i)\n" ;
-        tout << "        if (strcmp(str[i],name)==0)\n" ;
+        tout << "    for (i = 0; i < ns; ++i) {\n" ;
+        tout << "        if (strcmp(str[i], name)==0) {\n" ;
         tout << "            break;\n" ;
-        tout << "    if (i == ns)\n" ;
-        tout << "        return(-1);\n" ;
+        tout << "        }\n";
+        tout << "    }\n";
+        tout << "    if (i == ns) {\n" ;
+        tout << "        return -1;\n" ;
+        tout << "    }\n";
         tout << "    v[i] = atof(e);\n" ;
-        tout << "    return(i);\n" ;
-        tout << "    }\n" ;
+        tout << "    return i;\n" ;
+        tout << "}\n" ;
         tout << endl << endl;
         tout << "int main (int argc, char *argv[])\n" ;
-        tout << "    {\n" ;
+        tout << "{\n";
         tout << "    int i;\n" ;
         if (HasPi) {
             tout << "    const double Pi = M_PI;\n";
@@ -363,7 +369,7 @@ void VectorField::PrintGSL(map<string,string> options)
         tout << "};\n" ;
         tout << "    double y_[" << nv << "];\n" ;
 
-        tout << "    double solver_param_[3] = {1.0e-6,0.0,10.0};\n" ;
+        tout << "    double solver_param_[3] = {1.0e-6, 0.0, 10.0};\n" ;
 
         MakeCArrayOfStrings(tout,"varnames_",varname_list);
         if (np > 0) {
@@ -372,39 +378,36 @@ void VectorField::PrintGSL(map<string,string> options)
         else {
             tout << "    char *parnames_[] = {\"\"};\n";
         }
-        tout << "    char *solver_param_names_[3] = {\"abserr\",\"relerr\",\"stoptime\"};\n" ;
+        tout << "    char *solver_param_names_[3] = {\"abserr\", \"relerr\", \"stoptime\"};\n" ;
         tout << endl;
-        tout << "    for (i = 0; i < N_; ++i)\n" ;
+        tout << "    for (i = 0; i < N_; ++i) {\n" ;
         tout << "        y_[i] = def_y_[i];\n" ;
-        tout << "    for (i = 0; i < P_; ++i)\n" ;
+        tout << "    }\n";
+        tout << "    for (i = 0; i < P_; ++i) {\n" ;
         tout << "        p_[i] = def_p_[i];\n" ;
-        tout << "    for (i = 1; i < argc; ++i)\n" ;
-        tout << "        {\n" ;
+        tout << "    }\n";
+        tout << "    for (i = 1; i < argc; ++i) {\n" ;
         tout << "        int j;\n" ;
-        tout << "        if (strcmp(argv[i],\"-h\") == 0)\n" ;
-        tout << "            {\n" ;
-        tout << "            use(argc,argv,N_,varnames_,def_y_,P_,parnames_,def_p_);\n" ;
+        tout << "        if (strcmp(argv[i], \"-h\") == 0) {\n" ;
+        tout << "            use(argc, argv, N_, varnames_, def_y_, P_, parnames_, def_p_);\n" ;
         tout << "            exit(0);\n" ;
-        tout << "            }\n" ;
-        tout << "        j = assign(varnames_,N_,y_,argv[i]);\n" ;
-        tout << "        if (j == -1)\n" ;
-        tout << "            {\n" ;
-        tout << "            j = assign(parnames_,P_,p_,argv[i]);\n" ;
-        tout << "            if (j == -1)\n" ;
-        tout << "                {\n" ;
-        tout << "                j = assign(solver_param_names_,3,solver_param_,argv[i]);\n" ;
-        tout << "                if (j == -1)\n" ;
-        tout << "                    {\n" ;
-        tout << "                    fprintf(stderr,\"unknown argument: %s\\n\",argv[i]);\n" ;
-        tout << "                    use(argc,argv,N_,varnames_,def_y_,P_,parnames_,def_p_); \n";
+        tout << "        }\n" ;
+        tout << "        j = assign(varnames_, N_, y_, argv[i]);\n" ;
+        tout << "        if (j == -1) {\n" ;
+        tout << "            j = assign(parnames_, P_, p_, argv[i]);\n" ;
+        tout << "            if (j == -1) {\n" ;
+        tout << "                j = assign(solver_param_names_, 3, solver_param_, argv[i]);\n" ;
+        tout << "                if (j == -1) {\n" ;
+        tout << "                    fprintf(stderr, \"unknown argument: %s\\n\", argv[i]);\n" ;
+        tout << "                    use(argc, argv, N_, varnames_, def_y_, P_, parnames_, def_p_); \n";
         tout << "                    exit(-1);\n" ;
-        tout << "                    }\n" ;
         tout << "                }\n" ;
         tout << "            }\n" ;
         tout << "        }\n" ;
+        tout << "    }\n" ;
         tout << endl;
         tout << "    const gsl_odeiv_step_type *T_  = gsl_odeiv_step_rk8pd;\n" ;
-        tout << "    gsl_odeiv_step    *step_    = gsl_odeiv_step_alloc(T_,N_);\n" ;
+        tout << "    gsl_odeiv_step    *step_    = gsl_odeiv_step_alloc(T_, N_);\n" ;
         tout << "    gsl_odeiv_control *control_ = gsl_odeiv_control_y_new(solver_param_[0], solver_param_[1]);\n" ;
         tout << "    gsl_odeiv_evolve  *evolve_  = gsl_odeiv_evolve_alloc(N_);\n" ;
         tout << "    gsl_odeiv_system sys_ = {" << Name() << "_vf, " << Name() << "_jac, N_, &(p_[0])};\n";
@@ -413,31 +416,30 @@ void VectorField::PrintGSL(map<string,string> options)
         tout << "    double t1_ = solver_param_[2];\n" ;
         tout << "    double h_ = 1e-6;\n" ;
         tout << endl;
-        tout << "    while (t_ < t1_)\n" ;
-        tout << "        {\n" ;
+        tout << "    while (t_ < t1_) {\n" ;
         tout << "        int j_;\n" ;
         tout << "        int status_ = gsl_odeiv_evolve_apply(evolve_, control_, step_, &sys_, &t_, t1_, &h_, y_);\n" ;
-        tout << "        if (status_ != GSL_SUCCESS)\n" ;
-        tout << "            {\n" ;
-        tout << "            fprintf(stderr,\"status=%d\\n\",status_);\n" ;
+        tout << "        if (status_ != GSL_SUCCESS) {\n" ;
+        tout << "            fprintf(stderr, \"status=%d\\n\", status_);\n" ;
         tout << "            break;\n" ;
-        tout << "            }\n" ;
-        tout << "        printf(\"%.8e\",t_);\n" ;
-        tout << "        for (j_ = 0; j_ < N_; ++j_)\n" ;
-        tout << "            printf(\" %.8e\",y_[j_]);\n" ;
+        tout << "        }\n" ;
+        tout << "        printf(\"%.8e\", t_);\n" ;
+        tout << "        for (j_ = 0; j_ < N_; ++j_) {\n" ;
+        tout << "            printf(\" %.8e\", y_[j_]);\n" ;
+        tout << "        }\n";
         if (options["func"] == "yes") {
             for (int i = 0; i < nf; ++i) {
-                tout << "        printf(\" %.8e\"," << Name() << "_" << funcname_list[i] << "(t_,y_,p_));\n";
+                tout << "        printf(\" %.8e\", " << Name() << "_" << funcname_list[i] << "(t_, y_, p_));\n";
             }
         }
         tout << "        printf(\"\\n\");\n";
-        tout << "        }\n" ;
+        tout << "    }\n" ;
         tout << endl;
         tout << "    gsl_odeiv_evolve_free(evolve_);\n" ;
         tout << "    gsl_odeiv_control_free(control_);\n" ;
         tout << "    gsl_odeiv_step_free(step_);\n" ;
         tout << "    return 0;\n" ;
-        tout << "    }\n" ;
+        tout << "}\n" ;
         tout.close();
         //
         // Create a Makefile for the  GSL demo program
