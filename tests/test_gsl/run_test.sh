25f8f3c8b402f1d252b0a58a32451aa18bb7082c
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-20 18:52:58 -0400

    TEST: Fix typo; try DEBIAN_FRONTEND=noninteractive before apt-get

--- tests/test_gsl/run_test.sh
+++ tests/test_gsl/run_test.sh
@@ -1,11 +1,11 @@
 
 echo "Installing libgsl"
-apt-get -yqq install libgsl-dev
+DEBIAN_FRONTEND=noninteractive apt-get -yq install libgsl-dev
 
 echo "Running vfgen to generated code for gsl"
 vfgen gsl ../vf/linearosc.vf
 
-echo "Builing test_gsl"
+echo "Building test_gsl"
 gcc -c linearosc_gvf.c
 gcc -c test_gsl.c 
 gcc -o test_gsl test_gsl.o linearosc_gvf.o -lgsl -lgslcblas -lm
