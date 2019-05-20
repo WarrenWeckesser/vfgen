2e42cb63f1b679869b2f68736afc122aa299a7e7
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-20 17:26:42 -0400

    TEST: Add a test for lsoda.

---
+++ tests/test_lsoda/run_test.sh
@@ -0,0 +1,18 @@
+echo "Fetching LSODA from netlib."
+curl -O "http://www.netlib.org/odepack/{opkdmain.f,opkda1.f,opkda2.f}"
+
+echo "Running vfgen to generate Fortran routines for LSODA."
+vfgen lsoda ../vf/linearosc.vf
+
+echo "Building test_lsoda."
+gfortran -c -w opkdmain.f opkda1.f opkda2.f
+gfortran -c test_lsoda.f
+gfortran -c linearosc_rhs.f
+gfortran test_lsoda.o linearosc_rhs.o opkdmain.o opkda1.o opkda2.o -o test_lsoda
+
+echo "Running test_lsoda."
+./test_lsoda > out 2>&1
+
+if [ -s out ]; then
+    exit -1
+fi
---
+++ tests/test_lsoda/test_lsoda.f
@@ -0,0 +1,66 @@
+c
+c test_lsoda.f
+c
+c Fortran 77 program that uses LSODA to solve the differential equations
+c defined in the vector field 'linearosc'
+c
+c
+      program linearosc
+
+      implicit none
+
+      external linearosc_rhs
+      external linearosc_jac
+
+      double precision atol_, rtol_, y_, t_, tout_, tfinal_, rwork_
+      integer iwork_
+      dimension y_(2), rwork_(52), iwork_(22)
+      integer neq_, i_, j_, nsteps_
+      integer itol_, iopt_, itask_, istate_, jt_, lrw_, liw_
+      double precision x, y
+c     --- t range ---
+      t_ = 0.0D0
+      tfinal_  = 3.1415926535897932384626433D0
+      nsteps_ = 100
+c     --- Initial conditions ---
+      x = 1.0000000000000000D+00
+      y = 0.0000000000000000D+00
+      y_(1) = x
+      y_(2) = y
+c     --- Solver tolerances ---
+      rtol_ = 1.0D-9
+      atol_ = 1.0D-12
+      itol_ = 1
+c     --- Other LSODA parameters ---
+      neq_ = 2
+      itask_ = 1
+      istate_ = 1
+      iopt_ = 0
+      lrw_ = 52
+      liw_ = 22
+c
+c     LSODE and LSODA take the same arguments, so either may
+c     be used in the loop below.  jt_ must be set as follows:
+c     jt_ =  1 for LSODA
+c     jt_ = 10 for LSODE, non-stiff (Adams) method
+c     jt_ = 21 for LSODE, stiff (BDF) method
+c     See the documentation in the Fortran file for more details.
+      jt_ = 1
+
+c     --- Call DLSODA in a loop to compute the solution ---
+      do 40 i_ = 1,nsteps_
+          tout_ = (i_*tfinal_)/nsteps_
+          call DLSODA(linearosc_rhs, neq_, y_, t_, tout_,
+     &           itol_, rtol_, atol_, itask_, istate_, iopt_,
+     &           rwork_, lrw_, iwork_, liw_,
+     &           linearosc_jac, jt_)
+          if (istate_ .lt. 0) goto 80
+40    continue
+      if ((abs(y_(1) + 1) .gt. 1D-9) .or. (abs(y_(2)) .gt. 1D-9)) then
+          stop 'FAIL'
+      endif
+      stop
+80    write (6,89) istate_
+89    format(1X,"Error: istate=",I3)
+      stop 'FAIL' 
+      end
