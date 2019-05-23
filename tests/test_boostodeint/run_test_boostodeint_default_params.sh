e2f4ab51cb35cb7bae96d49b1ed986b3c6b94b31
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-22 22:52:09 -0400

    Fix printed output in run_test_boostodeint_default_params.sh.

--- tests/test_boostodeint/run_test_boostodeint_default_params.sh
+++ tests/test_boostodeint/run_test_boostodeint_default_params.sh
@@ -2,10 +2,10 @@
 echo "Running vfgen."
 vfgen boostodeint ../vf/linearoscp.vf
 
-echo "Building test_boostodeint_default_noparams."
+echo "Building test_boostodeint_default_params."
 g++ -c linearoscp_vf.cpp
 g++ -c test_boostodeint_default_params.cpp
 g++ test_boostodeint_default_params.o linearoscp_vf.o -o test_boostodeint_default_params
 
-echo "Running test_boostodeint_default_noparams."
+echo "Running test_boostodeint_default_params."
 ./test_boostodeint_default_params
