6c7c0ce4b86b75f79b514b8b44b9d1984095934d
Warren Weckesser <warren.weckesser@gmail.com>
2019-05-30 02:05:57 -0400

    dde_solver: Fix test messages, and include gfortran version in test output.

--- tests/test_dde_solver/run_test.sh
+++ tests/test_dde_solver/run_test.sh
@@ -4,12 +4,15 @@ curl -s -S -O https://raw.githubusercontent.com/WarrenWeckesser/dde_solver/maste
 echo "Running vfgen to generate Fortran routines for DDE_SOLVER."
 vfgen dde_solver ../vf/sdd.vf
 
-echo "Building test_lsoda."
+echo "Showing gfortran version."
+gfortran --version
+
+echo "Building test_dde_solver."
 gfortran -c dde_solver_m.f90
 gfortran -c sdd.f90
 gfortran test_dde_solver.f90 sdd.o dde_solver_m.o -o test_dde_solver
 
-echo "Running test_lsoda."
+echo "Running test_dde_solver."
 ./test_dde_solver > out 2>&1
 
 if [ -s out ]; then
