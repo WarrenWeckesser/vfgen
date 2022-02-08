04d99e27157a23fa641eec8c9973740c733e0dab
warren <warren.weckesser@gmail.com>
2022-02-08 15:14:54 -0500

    CI/github: Work on R test script.

--- tests/test_r/run_test.sh
+++ tests/test_r/run_test.sh
@@ -1,8 +1,5 @@
 
 echo "Installing R and the desolve package"
-apt-get -yqq install lsb_release
-deb https://ppa.launchpadcontent.net/marutter/c2d4u/ubuntu $(lsb_release -c -s) main
-deb-src https://ppa.launchpadcontent.net/marutter/c2d4u/ubuntu $(lsb_release -c -s) main
 sudo add-apt-repository ppa:marutter/c2d4u
 sudo apt-get update
 apt-get -y install r-cran-desolve
