6b466c2b90cee8e3d8a44330a80e479ba3086e08
warren <warren.weckesser@gmail.com>
2022-02-08 14:46:29 -0500

    CI/github: Work on R test script.

--- tests/test_r/run_test.sh
+++ tests/test_r/run_test.sh
@@ -1,7 +1,7 @@
 
 echo "Installing R and the desolve package"
 # update-alternatives --install /usr/bin/gfortran gfortran /usr/local/bin/gfortran 999
-apt-get -yqq install r-base-core r-cran-desolve
+apt-get -yqq install r-cran-desolve
 
 echo "Testing R"
 vfgen r ../vf/linearosc.vf
