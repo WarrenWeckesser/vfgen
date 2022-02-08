7726a30f1b565c74af2f56236ba30cb7c115ee98
warren <warren.weckesser@gmail.com>
2022-02-08 15:23:12 -0500

    CI/github: Work on R test script.

--- tests/test_r/run_test.sh
+++ tests/test_r/run_test.sh
@@ -1,8 +1,9 @@
 
 echo "Installing R and the desolve package"
-sudo add-apt-repository ppa:marutter/c2d4u
-sudo apt-get update
-apt-get -y install r-cran-desolve
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+apt-get update
+apt-get -y install r-base r-cran-desolve
 
 echo "Testing R"
 vfgen r ../vf/linearosc.vf
