8094517b3e9f50b8b9105d9bdce9ec0ab716c61e
warren <warren.weckesser@gmail.com>
2022-02-08 15:07:10 -0500

    CI/github: Work on R test script.

--- tests/test_r/run_test.sh
+++ tests/test_r/run_test.sh
@@ -1,6 +1,10 @@
 
 echo "Installing R and the desolve package"
-# update-alternatives --install /usr/bin/gfortran gfortran /usr/local/bin/gfortran 999
+apt-get -yqq install lsb_release
+deb https://ppa.launchpadcontent.net/marutter/c2d4u/ubuntu $(lsb_release -c -s) main
+deb-src https://ppa.launchpadcontent.net/marutter/c2d4u/ubuntu $(lsb_release -c -s) main
+sudo add-apt-repository ppa:marutter/c2d4u
+sudo apt-get update
 apt-get -y install r-cran-desolve
 
 echo "Testing R"
