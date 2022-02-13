abbc631065a59a879dedc81338cc0fac91a1fbcf
Warren Weckesser <warren.weckesser@gmail.com>
2022-02-13 15:28:20 -0500

    CI: Remove attempt to use cloud.r-project.org to install R packages

--- tests/test_r/run_test.sh
+++ tests/test_r/run_test.sh
@@ -1,8 +1,6 @@
 set -e
 
 echo "Installing R and the desolve package"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
 apt-get update
 apt-get -y install r-base r-cran-desolve
 
