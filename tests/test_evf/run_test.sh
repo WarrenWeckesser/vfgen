415847bb34420a5ca2db5b6a36158ff14d4d4a79
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-08 19:21:01 -0400

    CI: Start work on more modular testing.

--- tests/test_evf/run_test.sh
+++ tests/test_evf/run_test.sh
@@ -1,8 +1,7 @@
 set -e
 
-## gsl should already be installed when this script is run.
-#echo "Installing libgsl"
-#apt-get -yq install libgsl-dev
+echo "Installing libgsl"
+apt-get -yq install libgsl-dev
 
 echo "Running vfgen evf ../vf/rossler.vf > rossler_evf.vf"
 vfgen evf ../vf/rossler.vf > rossler_evf.vf
