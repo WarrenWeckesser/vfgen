c1521e575df75400db57b87ef8006832bb114c43
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-08 21:04:58 -0400

    CI: Fix noninteractive installation of julia.
    [test julia]

--- tests/test_julia/run_test.sh
+++ tests/test_julia/run_test.sh
@@ -1,7 +1,7 @@
 set -e
 
 echo "Installing Julia"
-curl -fsSL https://install.julialang.org | sh -- -y
+curl -fsSL https://install.julialang.org | sh -s -- -y
 
 echo "Installing Julia DifferentialEquations package"
 julia -e 'import Pkg; Pkg.add("DifferentialEquations")'
