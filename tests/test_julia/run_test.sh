451c7bc5f670df21cbf3cc9aded5804afc0922ea
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-10 14:07:14 -0400

    TST: julia: Split script into separate install and test scripts.
    [test julia]

--- tests/test_julia/run_test.sh
+++ tests/test_julia/run_test.sh
@@ -1,11 +1,5 @@
 set -e
 
-echo "Installing Julia"
-curl -fsSL https://install.julialang.org | sh -s -- -y
-
-echo "Installing Julia DifferentialEquations package"
-julia -e 'import Pkg; Pkg.add("DifferentialEquations")'
-
 echo "Testing julia"
 vfgen julia ../vf/linearosc.vf
 julia test_julia.jl
