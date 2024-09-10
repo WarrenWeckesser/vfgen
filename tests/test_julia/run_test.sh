87b9283e7fad7c969a9bfe346985cf7d8388a249
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-10 16:49:35 -0400

    TST: julia: fix test.
    [test julia]

--- tests/test_julia/run_test.sh
+++ tests/test_julia/run_test.sh
@@ -7,7 +7,7 @@ echo "Running test_linearosc.jl"
 julia test_linearosc.jl
 
 echo "Generating from linearoscp.vf"
-vfgen julia ../vf/linearosc.vf
+vfgen julia:func=yes ../vf/linearoscp.vf
 
 echo "Running test_linearoscp.jl"
 julia test_linearoscp.jl
