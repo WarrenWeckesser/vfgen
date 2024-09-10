28fd73fead451c9c05d4f8346bfdbd3c167f508c
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-09 23:50:30 -0400

    CI: Try testing pygsl from github with numpy 1.26.4.
    [test pygsl]

--- tests/test_pygsl/run_test.sh
+++ tests/test_pygsl/run_test.sh
@@ -3,9 +3,16 @@ set -e
 echo "Installing swig, setuptools, wheel, numpy and pygsl"
 apt-get -yqq install swig
 python3 -m pip install setuptools wheel
-python3 -m pip install numpy
-python3 -m pip install pygsl
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install numpy==1.26.4
+git clone https://github.com/pygsl/pygsl.git
+cd pygsl
+pip3 install .
+cd ..
 
 echo "Testing pygsl"
 vfgen pygsl ../vf/linearosc.vf
 python3 test_pygsl.py
+
+deactivate
