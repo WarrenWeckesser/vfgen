7084dd54ea2fa1e7fc34337fef6c219e685feea1
Warren Weckesser <warren.weckesser@gmail.com>
2024-09-20 17:31:34 -0400

    CI: fix name displayed in the adolc test script run_test.sh
    [test adolc]

--- tests/test_adolc/run_test.sh
+++ tests/test_adolc/run_test.sh
@@ -1,12 +1,12 @@
 set -e
 
-echo "Generating for linearosc.vf"
+echo "Generating for linearoscp.vf"
 vfgen adolc ../vf/linearoscp.vf
 
-echo "Building test_linearosc"
+echo "Building test_linearoscp"
 g++ -c linearoscp_adolc.cpp
 g++ -c test_linearoscp.cpp
 g++ -o test_linearoscp test_linearoscp.o linearoscp_adolc.o -ladolc -lm
 
-echo "Running test_linearosc"
+echo "Running test_linearoscp"
 ./test_linearoscp
